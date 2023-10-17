a8084d26efcc66d8359bace43086df411472055c
Yasine31130 <yasine.bns@hotmail.com>
2023-10-17 11:48:31 -0700

    5. Sign

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,20 +1,20 @@
 #include "main.h"
 /**
- * print_sign - check the code.
- * @n: var int
- * Return: 1 if positive 0 if zero and -1 if negative
+ * print_sign - Description
+ * @n: variable
+ * Return: if n positive return
  */
 int print_sign(int n)
 {
 	if (n > 0)
 	{
-		 _putchar(43);
-		 return (1);
+		_putchar(43);
+		return (1);
 	}
 	else if (n < 0)
 	{
-		 _putchar(45);
-		 return (-1);
+		_putchar(45);
+		return (-1);
 	}
 	else if (n == 0)
 	{
