16c1831df94db7508adce9722e3e260d832e6a5b
Yasine31130 <yasine.bns@hotmail.com>
2023-10-17 09:23:59 -0700

    0. _putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - DEscription
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("_putchar");
+	return (0);
+}
