336109cd3d628cc1d9e4917afa5cafe0d0e84dad
Yasine31130 <yasine.bns@hotmail.com>
2023-10-17 09:38:16 -0700

    1. I sometimes suffer from insomnia. And when I can t fall asleep, I play what I call the alphabet game

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * print_alphabet - check the code
  *
  * Return: Always 0.
  */
@@ -15,5 +15,4 @@ void print_alphabet(void)
 		a++;
 	}
 	_putchar('\n');
-	return (0);
 }
