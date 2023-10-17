3d5412e68ba2ff41862f8d9db0fc870817033a46
Yasine31130 <yasine.bns@hotmail.com>
2023-10-17 11:06:31 -0700

    3. islower

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_alphabet - check the code
+ *
+ * Return: Always 0.
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return(1);
+	}
+	else
+		return(0);
+}
