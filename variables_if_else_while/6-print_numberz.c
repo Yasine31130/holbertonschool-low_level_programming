7c27bc5948aef665f31d7e8a6b1d139daa5c9039
Yasine31130 <yasine.bns@hotmail.com>
2023-10-17 08:53:37 -0700

    6. Numberz

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - DEscription
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char n = 48;
+
+	while (n <= 57)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
