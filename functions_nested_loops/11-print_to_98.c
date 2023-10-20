273ec93901268f07b54c031ebd701303f345d21b
Yasine31130 <yasine.bns@hotmail.com>
2023-10-20 09:38:09 -0700

    11. 98 Battery Street, the OG

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_to_98 - Description
+ * @n: var num
+ * Return: prints all natural numbers from n to 98, followed by a new line
+ */
+void print_to_98(int n)
+{
+	while (n >= 98)
+	{
+		printf("%d, ",n);
+		n--;
+	}
+	while (n <= 98)
+	{
+		printf("%d, ",n);
+		n++;
+	}
+	putchar('\n');
+}
