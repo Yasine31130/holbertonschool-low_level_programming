fb595b246a366d79bc7981b545cb4215d4252ba7
Yasine31130 <yasine.bns@hotmail.com>
2023-10-21 10:04:31 -0700

    9. Fizz-Buzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - description
+ * Return: prints the numbers from 1 to 100
+ */
+int main(void)
+{
+	int i = 1;
+
+	while (i < 100)
+	{
+		int r = i % 3;
+		int q = i % 5;
+
+		if (r == 0)
+		{
+			printf("Fizz");
+			if (q == 0)
+			{
+				printf("Buzz");
+			}
+			i++;
+			putchar(' ');
+		}
+		else if (q == 0)
+		{
+			printf("Buzz");
+			i++;
+			putchar(' ');
+		}
+		else
+		{
+			printf("%d", i);
+			putchar(' ');
+			i++;
+		}
+	}
+}
