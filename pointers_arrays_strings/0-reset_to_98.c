91fda2b4f4c989bcf8061d35983113d19d111a6c
Yasine31130 <yasine.bns@hotmail.com>
2023-10-23 01:23:53 -0700

    C - Pointers, arrays and strings

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reset_to_98 - description
+ * @n: pointer
+ * Return: updates the value it points to to 98.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+
+	return (n);
+}
