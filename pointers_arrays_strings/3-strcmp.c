6099e2e6872685cd68c846595d35fb5b1327226e
Yasine31130 <yasine.bns@hotmail.com>
2023-10-28 10:31:28 -0700

    3. strcmp

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,17 @@
+#include"main.h"
+/**
+ * _strcmp - description
+ * @s1: pointer to string
+ * @s2: pointer to second string
+ * Return: function that compares two strings
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while ((s2[i] && s1[i]) == s2[i])
+	{
+		++i;
+	}
+	return (s1[i] - s2[i]);
+}
