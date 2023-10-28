e69c24300196b0524fc6905a397c731831d578c9
Yasine31130 <yasine.bns@hotmail.com>
2023-10-28 07:26:20 -0700

    2. strncpy

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -16,8 +16,8 @@ char *_strncpy(char *dest, char *src, int n)
 		i++;
 	}
 	while (i < n)
-                {
-                        dest[i++] = '\0';
-                }
+	{
+		dest[i++] = '\0';
+	}
 	return(dest);
 }
