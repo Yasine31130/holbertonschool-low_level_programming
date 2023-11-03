71d404f9134b0afed866de15231fef6d9c88f349
Yasine31130 <yasine.bns@hotmail.com>
2023-11-03 09:48:34 -0700

    4. To infinity and beyond

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -19,9 +19,9 @@ int main(int argc, char *argv[])
 		j = 0;
 		while (s[j] != '\0')
 		{
-			if (s[j] < '0' || s[i] > '9')
+			if (s[j] < '0' || s[j] > '9')
 			{
-				printf("Error");
+				printf("Error\n");
 				return (1);
 			}
 			j++;
@@ -31,7 +31,7 @@ int main(int argc, char *argv[])
 	i = 1;
 	while (i < argc)
 	{
-		result += atoi(argv[i]);
+		result = result + atoi(argv[i]);
 		i++;
 	}
 	printf("%d\n", result);
