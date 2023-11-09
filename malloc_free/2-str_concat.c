53e9d33a55333e01a3a33f25adc8438e9a580afb
Yasine31130 <yasine.bns@hotmail.com>
2023-11-09 10:01:28 -0800

    2. He who is not courageous enough to take risks will accomplish nothing in life

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * str_concat - description
+ * @s1: pointer to string
+ * @s2: pointer to second string
+ * Return: function that concatenates two strings.
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int i = 0;
+	int q = 0;
+	char *ptr = NULL;
+
+	if (s1 == NULL && s2 == NULL)
+	{
+		return (NULL);
+	}
+	while (s1[i] != '\0')
+	{
+		i++;
+	}
+	while (s2[q] != '\0')
+	{
+		q++;
+	}
+	q++;
+	ptr = malloc((i + q) * sizeof(char));
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		ptr[i] = s1[i];
+	}
+	q = 0;
+	while (s2[q] != '\0')
+	{
+		ptr[i] = s2[q];
+		i++;
+		q++;
+	}
+
+	return (ptr);
+}
