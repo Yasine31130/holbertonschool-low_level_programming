009dc593dad2e2b23499139849ed6131bc9e04f2
Yasine31130 <yasine.bns@hotmail.com>
2023-11-09 10:07:40 -0800

    2. He who is not courageous enough to take risks will accomplish nothing in life

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -12,9 +12,13 @@ char *str_concat(char *s1, char *s2)
 	int q = 0;
 	char *ptr = NULL;
 
-	if (s1 == NULL && s2 == NULL)
+	if (s1 == NULL)
 	{
-		return (NULL);
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
 	}
 	while (s1[i] != '\0')
 	{
