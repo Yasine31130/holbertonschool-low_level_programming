d460fd0923832fb93780699bb8363914f6f6cb81
Yasine31130 <yasine.bns@hotmail.com>
2023-11-09 06:06:44 -0800

    0. Float like a butterfly, sting like a bee

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -9,6 +9,7 @@
 char *create_array(unsigned int size, char c)
 {
 	char *array;
+	unsigned int i;
 
 	if (size == 0)
 	{
@@ -18,7 +19,7 @@ char *create_array(unsigned int size, char c)
 	if (array == NULL)
 		return (NULL);
 
-	for (unsigned int i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		array[i] = c;
 	}
