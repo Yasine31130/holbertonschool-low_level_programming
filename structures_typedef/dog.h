48d9d3f6b2b4a9bde870a8cb3dc6916ddac569f2
Yasine31130 <yasine.bns@hotmail.com>
2023-11-13 06:21:04 -0800

    C - Structures, typedef

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - Description
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Description: Longer description
+ */
+typedef struct dog
+{
+	name = char *
+	age = float
+	owner = char *
+}
+#endif
