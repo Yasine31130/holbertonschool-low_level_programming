f82949874e307a033df81d5fc2eab78cfd79d490
Yasine31130 <yasine.bns@hotmail.com>
2023-11-14 05:08:48 -0800

    4. A door is what a dog is perpetually on the wrong side of

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -4,57 +4,57 @@
  * _strdup - duplicate a string of char
  * @str: pointer to a string
  * Return: pointer to a string
-*/
+ */
 
-char _strdup(charstr);
+char *_strdup(char *str);
 
-char _strdup(charstr)
+char *_strdup(char *str)
 {
-        char *s;
+	char *s;
 
-        if (str == NULL)
-        {
-                return (NULL);
-        }
-        s = malloc(strlen(str) + 1);
-        if (s == NULL)
-        {
-                return (NULL);
-        }
-        {
-                strcpy(s, str);
-        }
-        return (s);
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	s = malloc(strlen(str) + 1);
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	{
+		strcpy(s, str);
+	}
+	return (s);
 }
- 
+
 /**
  * dog_t *new_dog - creates a new dog
  * @name: name of new dog
  * @age: age of new dog
  * @owner: owner of new dog
  * Return: pointer to new dog
-*/
+ */
 
-dog_t new_dog(charname, float age, char owner)
+dog_t *new_dog(char *name, float age, char *owner)
 {
-        dog_tnew_dog;
+	dog_t *new_dog;
 
-        new_dog = malloc(sizeof(dog_t));
-        if (new_dog == NULL)
-                return (NULL);
-        new_dog->name = _strdup(name);
-        if (new_dog->name == NULL)
-        {
-                free(new_dog);
-                return (NULL);
-        }
-        new_dog->owner = _strdup(owner);
-        if (new_dog->owner == NULL)
-        {
-                free(new_dog->name);
-                free(new_dog);
-                return (NULL);
-        }
-        new_dog->age = age;
-        return (new_dog);
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
+		return (NULL);
+	new_dog->name = _strdup(name);
+	if (new_dog->name == NULL)
+	{
+		free(new_dog);
+		return (NULL);
+	}
+	new_dog->owner = _strdup(owner);
+	if (new_dog->owner == NULL)
+	{
+		free(new_dog->name);
+		free(new_dog);
+		return (NULL);
+	}
+	new_dog->age = age;
+	return (new_dog);
 }
