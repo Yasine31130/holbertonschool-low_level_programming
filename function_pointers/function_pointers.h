7db4f3c004247d7f99c53fa9604b903891166466
Yasine31130 <yasine.bns@hotmail.com>
2023-11-14 06:31:02 -0800

    0. What s my name

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,4 +1,6 @@
-#ifndef FUNCTION_POINTER_H
-#define FUNCTION_POINTER_H
+#ifndef FUNCTION_POINTER
+#define FUNCTION_POINTER
+#include <stdio.h>
+#include <stdlib.h>
 void print_name(char *name, void (*f)(char *));
 #endif
