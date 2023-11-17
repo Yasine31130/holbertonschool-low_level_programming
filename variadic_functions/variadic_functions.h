d5cdf7a563ec66cd68f50c9f846698c91607fce2
Yasine31130 <yasine.bns@hotmail.com>
2023-11-17 15:01:49 -0800

    3. To be is a to be the value of a variable

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -13,8 +13,13 @@ typedef struct formats
 	char ch;
 	void (*func)(va_list);
 } ft;
+
 int sum_them_all(const unsigned int n, ...);
+
 void print_numbers(const char *separator, const unsigned int n, ...);
+
 void print_strings(const char *separator, const unsigned int n, ...);
+
 void print_all(const char * const format, ...);
+
 #endif
