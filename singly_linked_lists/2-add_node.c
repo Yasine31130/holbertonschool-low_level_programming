af3a9607085b335b19a0f7c02e714cb469911729
Yasine31130 <yasine.bns@hotmail.com>
2023-12-01 05:46:11 -0800

    2. Add node

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+ * add_node - descritpion
+ * @head: head of node
+ * @str: sring inside node
+ * Return: Return: the address of the new element, or NULL if it failed
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free (new_node);
+		return (NULL);
+	}
+	new_node->len = strlen(str);
+	new_node->next = *head;
+	*head = new_node;
+	return (*head);
+}
--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -19,4 +19,5 @@ typedef struct list_s
 } list_t;
 
 size_t print_list(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
 #endif
