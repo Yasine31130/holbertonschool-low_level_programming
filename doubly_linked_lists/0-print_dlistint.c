1f72de4b86323a779325ee943ea21fcf4c711f68
Yasine31130 <yasine.bns@hotmail.com>
2023-12-01 10:16:25 -0800

    4. Free list

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -1,30 +1,23 @@
 #include "lists.h"
+
 /**
- * print_dlistint - description
- * @h: pointer to node
- * Return: he number of nodes
+ * print_dlistint - prints all elements of a dlistint_t
+ * @h: a const list_t pointer to the head of a list (first element)
+ *
+ * Return: the size_t number of nodes in the list
  */
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t count;
-	const list_t *node;
+	const dlistint_t *node;
 
 	if (h == NULL)
-	{
 		return (0);
-	}
 	node = h;
 	count = 0;
 	while (node != NULL)
 	{
-		if (node->str == NULL)
-		{
-			printf("[0] (nil)\n");
-		}
-		else
-		{
-			printf("[%u] %s\n", node->len, node->str);
-		}
+		printf("%d\n", node->n);
 		node = node->next;
 		count++;
 	}
--- doubly_linked_lists/1-list_len.c
+++ doubly_linked_lists/1-list_len.c
@@ -1,16 +1,18 @@
 #include "lists.h"
 
 /**
- * list_len - returns the number of elements in a list_t
+ * dlistint_len - returns the number of elements in a dlist_int list
  * @h: a const list_t pointer to the head of a list (first element)
  *
- * Return: the size_t number of nodes in the list
+ * Return: the size_t number of elements in the list
  */
-size_t list_len(const list_t *h)
+size_t dlistint_len(const dlistint_t *h)
 {
-	size_t count = 0;
-	const list_t *node = h;
+	size_t count;
+	const dlistint_t *node;
 
+	node = h;
+	count = 0;
 	while (node != NULL)
 	{
 		node = node->next;
